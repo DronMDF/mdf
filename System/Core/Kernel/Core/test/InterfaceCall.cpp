24c12f7a38adddcc048d7bae5e926a71083094cc
Andrey Valyaev <dron@infosec.ru>
2009-11-05 08:13:23 +0300

    Раскомментировал тесты, но тест с нитями почему-то не проходит.. думать надо

--- System/Core/Kernel/Core/test/InterfaceCall.cpp
+++ System/Core/Kernel/Core/test/InterfaceCall.cpp
@@ -9,8 +9,8 @@
 #include "Types.h"
 #include "../Kernel.h"
 #include "../Core.h"
-#include "../Scheduler.h"
 
+#include "testScheduler.h"
 #include "testProcess.h"
 #include "testThread.h"
 
@@ -25,26 +25,28 @@ BOOST_AUTO_TEST_CASE(testInvalidId)
 	BOOST_REQUIRE_EQUAL(CoreCall(0, invalid_id, 0, 0, 0), ERROR_INVALIDID);
 }
 
-// Этот тест ведет себя нестабильно... не могу понять в чем проблема.
-// BOOST_AUTO_TEST_CASE(testCallProcessAsyncWithoutCaller)
-// {
-// 	const laddr_t entry = 6666;
-// 	testProcess process(entry);
-// 	
-// 	BOOST_REQUIRE_EQUAL(CoreCall(0, process.getId(), 0, 0, RESOURCE_CALL_ASYNC), SUCCESS);
-// 
-// 	ResourceThread *thread = Scheduler().getThread();
-// 	BOOST_REQUIRE(thread != 0);
-// 
-// 	BOOST_REQUIRE_EQUAL(thread->getProcess(), &process);
-// 	BOOST_REQUIRE_EQUAL(thread->getEntry(), entry);
-// }
+BOOST_AUTO_TEST_CASE(testCallProcessAsyncWithoutCaller)
+{
+	const laddr_t entry = 6666;
+	testProcess process(entry);
+	
+	BOOST_REQUIRE_EQUAL(CoreCall(0, process.getId(), 0, 0, RESOURCE_CALL_ASYNC), SUCCESS);
 
-// BOOST_AUTO_TEST_CASE(testCallThreadAsyncWithoutCaller)
-// {
-// 	testThread thread;
-// 	BOOST_REQUIRE_EQUAL(CoreCall(0, thread.getId(), 0, 0, RESOURCE_CALL_ASYNC), SUCCESS);
-// 	BOOST_REQUIRE(Scheduler().getThread() != &thread);
-// }
+	testScheduler scheduler;
+	ResourceThread *thread = scheduler.getThread();
+	BOOST_REQUIRE(thread != 0);
+
+	BOOST_REQUIRE_EQUAL(thread->getProcess(), &process);
+	BOOST_REQUIRE_EQUAL(thread->getEntry(), entry);
+}
+
+BOOST_AUTO_TEST_CASE(testCallThreadAsyncWithoutCaller)
+{
+	testThread thread;
+	BOOST_REQUIRE_EQUAL(CoreCall(0, thread.getId(), 0, 0, RESOURCE_CALL_ASYNC), SUCCESS);
+
+	testScheduler scheduler;
+	BOOST_REQUIRE(scheduler.getThread() != &thread);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
