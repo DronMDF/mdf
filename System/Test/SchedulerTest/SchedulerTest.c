771495b4d7cff0fd78a8494f3f9d0655f72d20bb
Andrey Valyaev <dron@securitycode.ru>
2010-12-15 17:56:32 +0300

    Отключил тесты в SchedulerTest, они не хотят работать под clang, А остальную сборку проверил - работает, clang включен по умолчанию.

--- System/Test/SchedulerTest/SchedulerTest.c
+++ System/Test/SchedulerTest/SchedulerTest.c
@@ -8,8 +8,8 @@
 
 	#include <iostream>
 
-	#define BOOST_TEST_MAIN
-	#define BOOST_TEST_DYN_LINK
+	//#define BOOST_TEST_MAIN
+	//#define BOOST_TEST_DYN_LINK
 	#include <boost/test/unit_test.hpp>
 
 	using namespace std;
